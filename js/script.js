5addfc91547ba60fdf70623df9bed1d55e21e305
Saumya Pandey <sp160899@gmail.com>
2019-12-26 15:41:22 +0530

    Fix the GreyScale bug

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let resizebtn = document.getElementById("resize");
 let blackbtn = document.getElementById("black");
 let clearSketchbtn = document.getElementById("clearSketch");
-let randomColors = document.getElementById("randomColors");
+let randomColorsbtn = document.getElementById("randomColors");
 let greyScalebtn = document.getElementById("greyScale");
 
 
@@ -9,7 +9,7 @@ greyScalebtn.addEventListener("click", greyScaleFilling);
 resizebtn.addEventListener("click", constructGrid);
 blackbtn.addEventListener("click", fillBoxes);
 clearSketchbtn.addEventListener("click", clearBoxes);
-randomColors.addEventListener("click", fillBoxesRandom);
+randomColorsbtn.addEventListener("click", fillBoxesRandom);
 
 function constructGrid(e) {
     const container = document.getElementById("container");
@@ -33,7 +33,6 @@ function constructGrid(e) {
     }
     container.style.gridTemplateColumns = string;
     container.style.gridTemplateRows = string;
-    fillBoxes();
 }
 
 function clearBoxes(e) {
@@ -55,35 +54,49 @@ function getRandomColor() {
 
 function fillBoxes(e) {
     clearBoxes();
+    clearListeners();
     let boxes = document.querySelectorAll(".box");
-    boxes.forEach(box => box.addEventListener("mouseover", function (e) {
-        box.style.backgroundColor = "black";
-    }));
+    boxes.forEach(box => box.addEventListener("mouseover",blackColor));
 }
 
+function blackColor(e) {
+        this.style.backgroundColor = "black";
+}
 function fillBoxesRandom(e) {
     clearBoxes();
+    clearListeners();
     let boxes = document.querySelectorAll(".box");
-    boxes.forEach(box => box.addEventListener("mouseover", function (e) {
-        box.style.backgroundColor = getRandomColor();
-    }));
+    boxes.forEach(box => box.addEventListener("mouseover",randomColors));
+}
+
+function randomColors(e) {
+        this.style.backgroundColor = getRandomColor();
 }
 
 function greyScaleFilling(e) {
+    
     clearBoxes();
-    let colors = ["rgb(255, 255, 255)", "rgb(230, 230, 230)", "rgb(204, 204, 204)", "rgb(179, 179, 179)", "rgb(153, 153, 153)", "rgb(128, 128, 128)", "rgb(102, 102, 102)", "rgb(77, 77, 77)"
-                  , "rgb(51, 51, 51)", "rgb(26, 26, 26)", "rgb(0, 0, 0)"]
+    clearListeners();
     let boxes = document.querySelectorAll(".box");
-    boxes.forEach(box => box.addEventListener("mouseover", function (e) {
-        let element = getComputedStyle(box);
-        console.log(element);
+    boxes.forEach(box => box.addEventListener("mouseover", greyColor));
+}
+
+function greyColor(e){
+        let colors = ["rgb(255, 255, 255)", "rgb(230, 230, 230)", "rgb(204, 204, 204)", "rgb(179, 179, 179)", "rgb(153, 153, 153)", "rgb(128, 128, 128)", "rgb(102, 102, 102)", "rgb(77, 77, 77)", "rgb(51, 51, 51)", "rgb(26, 26, 26)", "rgb(0, 0, 0)"]
+        let element = getComputedStyle(this);
         let col = element.backgroundColor;
         console.log(col);
         for (let i = 0; i < 10; i++) {
-            if (colors[i] === col) {
-                box.style.backgroundColor = colors[i + 1];
+            if (colors[i] == col) {
+                this.style.backgroundColor = colors[i + 1];
                 break;
             }
         }
-    }));
+}
+
+function clearListeners(){
+        let boxes = document.querySelectorAll(".box");
+        boxes.forEach(box => box.removeEventListener("mouseover",blackColor));
+        boxes.forEach(box => box.removeEventListener("mouseover",randomColors));
+        boxes.forEach(box => box.removeEventListener("mouseover",greyColor));
 }
